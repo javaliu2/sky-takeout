a5d4a48c7342ff98dd8b33f7d6794148683e8e03
xs <2359035384@qq.com>
2025-03-04 11:41:39 +0800

    员工注册功能实现，controller层负责页面逻辑跳转，service层负责具体功能的实现，mapper层负责数据持久化

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +11,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -54,4 +59,29 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employee;
     }
 
+    /**
+     * 员工注册
+     * @param employeeDTO
+     */
+
+    @Override
+    public void register(EmployeeDTO employeeDTO) {
+        // 保存的是entity，因此需要设置entity对象的属性
+        // 由于 EmployeeDTO 是 Employee 的子类，属性一致，因此使用spring提供的库函数完成属性的赋值
+        Employee newEmployee = new Employee();
+        BeanUtils.copyProperties(employeeDTO, newEmployee);
+        // 对 Employee 的其他属性进行赋值
+        newEmployee.setCreateTime(LocalDateTime.now());
+        newEmployee.setUpdateTime(LocalDateTime.now());
+//        newEmployee.setId(1);  // 不用设置，因为id是由数据库在管理维护的，这也减少了业务代码的复杂度，体现了功能分层的思想
+        // TODO 后面需要获取当前进行员工注册操作人的id
+        newEmployee.setCreateUser(10L);  // 需要追加'L'以表明这是long数据类型的数
+        newEmployee.setUpdateUser(10L);
+
+        newEmployee.setStatus(StatusConstant.ENABLE);  // 将常量提取出来管理，便于后期维护
+        newEmployee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));  // 同理
+
+        // 持久层操作
+        employeeMapper.insert(newEmployee);
+    }
 }
