0823d84877e1a3dc5baaab503af9c885f77f5510
xs <2359035384@qq.com>
2025-03-04 17:30:10 +0800

    员工分页查询 1、借助PageHelper完成查询，其通过pageSize和page_num计算LIMIT的参数，进而查询到page_num页的数据（sql语句中一个';'表示语句的结束，.xml中最好不要使用，因为可能mybatis会自动生成一些sql语句追加到我们写的后面，有';'的话，就是两条语句了，会发生语法错误的问题，也不绝对，就是记录一下） 2、可以通过@JsonFormat完成日期数据类型变量的格式化，或者重写WebMvcConfigurationSupport中的extendMessageConverters方法，一般采用第二种方式，因为它可以应用于全局

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,8 +3,10 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
@@ -13,10 +15,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -83,5 +82,22 @@ public class EmployeeController {
         log.info("员工注册：{}", employeeDTO);
         employeeService.register(employeeDTO);
         return Result.success();
+    }
+
+    /**
+     * 他是query，所以形参不要加@RequestBody
+     * @param employeePageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("员工分页查询")
+    public Result<PageResult> pageShow(EmployeePageQueryDTO employeePageQueryDTO) {
+        /**
+         * 前端提供的数据：当前页数，每页展示数据的条数，员工姓名
+         * 后端响应的数据：请求页数据列表
+         */
+        log.info("员工分页查询：{}", employeePageQueryDTO);
+        PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
+        return Result.success(pageResult);
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,16 +1,20 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -86,5 +91,22 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         // 持久层操作
         employeeMapper.insert(newEmployee);
+    }
+
+    /**
+     * 员工分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    @Override
+    public PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO) {
+        // 借助PageHelper完成分页查询功能的实现
+        // 通过mysql提供的库函数limit完成指定页数数据的查询
+        PageHelper.startPage(employeePageQueryDTO.getPage(), employeePageQueryDTO.getPageSize());
+        Page<Employee> data = employeeMapper.pageQuery(employeePageQueryDTO);
+
+        long total = data.getTotal();
+        List<Employee> records = data.getResult();
+        return new PageResult(total, records);
     }
 }
